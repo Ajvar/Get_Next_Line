c4706aabcfdfe13a7cd8a5e2ab41367cd393e758
Joscelyn CUEILLE <jcueille@e1r5p1.42.fr>
2019-11-25 18:08:24 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,37 @@
 /*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/25 13:56:20 by jcueille          #+#    #+#             */
-/*   Updated: 2019/11/15 11:38:58 by jcueille         ###   ########.fr       */
+/*   Updated: 2019/11/25 18:04:52 by jcueille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <string.h>
+
+int ft_buffcheck(char** buff, char** line, char** tmp, size_t i)
+{
+	if (*buff)
+	{
+		free(line[0]);
+		if (ft_strchr(*buff, '\n'))
+		{
+			while (buff[0][i] != '\n' && buff[0][i])
+				i++;
+			line[0] = ft_substr(*buff, 0, i);
+			*tmp = ft_strdup(ft_strchr(*buff, '\n') + 1);
+			free(*buff);
+			*buff = NULL;
+			*buff = ft_substr(*tmp, 0, ft_strlen(*tmp));
+			free(*tmp);
+			return (1);
+		}
+		line[0] = strdup(*buff);
+		//free(buff);
+		*buff = NULL;
+	}
+	return (0);
+}
 
 int				get_next_line(int fd, char **line)
 {
@@ -23,29 +47,14 @@ int				get_next_line(int fd, char **line)
 	size_t		i;
 
 	i = 0;
+	tmp = NULL;
 	if (line == NULL || fd < 0 || BUFFER_SIZE < 1)
 		return (-1);
 	if (!(line[0] = ft_strdup("")))
 		return (-1);
-/*----------SI QQ CHOSE DANS TAMPON--------------*/
-	if (buff)
-	{
-		free(line[0]);
-		if (ft_strchr(buff, '\n'))
-		{
-			while (buff[i] != '\n' && buff[i])
-				i++;
-			line[0] = ft_substr(buff, 0, i);
-			buff = ft_strdup(ft_strchr(buff, '\n') + 1);
-			return (1);
-		}
-		line[0] = strdup(buff);
-		free(buff);
-		//*buff = (char)NULL;
-	}
-/*---------------------*/
+	if (ft_buffcheck(&buff, line, &tmp, i) == 1)
+		return (1);
 	str = ft_calloc((BUFFER_SIZE + 1), 1);
-	//buff = ft_strdup("");
 	while ((ret = read(fd, str, BUFFER_SIZE)))
 	{
 		if (ret == -1)
@@ -72,28 +81,24 @@ int				get_next_line(int fd, char **line)
 	free(str);
 	if (ret > 0)
 		return (1);
-/*	if (*buff)
-	{
-		free(buff);
-		*buff = (char)NULL;
-	}*/
 	return (0);
 }
-/*
-int main(void)
+
+/*int main(void)
 {
 	int fd;
 	fd = open("tests.txt", O_RDWR);
 	char **line;
 	line = malloc(sizeof(line) * 10);
 	int n = 1;
-	while (n > 0)
-	{
-		n = get_next_line(fd, line);
-		printf("%s\n",line[0]);
-		free(line[0]);
-	}
+	//while (n > 0)
+	//{
+	n = get_next_line(fd, line);
+	printf("%s\n",line[0]);
+	free(line[0]);
+	//}
 	close(fd);
+	while(1);
 	return (0);
 }*/
 /*
